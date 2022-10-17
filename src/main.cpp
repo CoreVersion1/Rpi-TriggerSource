313259a659294e263597c52042cd4e7ee45a9d5b
Meng.Zhou <meng.zhou@xyzrobotics.ai>
2022-10-17 09:16:54 +0800

    unify code format

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,23 @@
 #include <pigpio.h>
 #include <glog/logging.h>
 
-int main(int argc,char** argv)
+int main(int argc, char **argv)
 {
   int gpio_cfg = gpioCfgGetInternals();
-  gpio_cfg |= PI_CFG_NOSIGHANDLER;  // (1<<10)
+  gpio_cfg |= PI_CFG_NOSIGHANDLER; // (1<<10)
   gpioCfgSetInternals(gpio_cfg);
   int status = gpioInitialise();
-  LOG(INFO) << "gpio init = "<< status;
-  
+  LOG(INFO) << "gpio init = " << status;
+
   LOG(INFO) << "gpio GPIO_TRIGGER_OUT output    = " << gpioSetMode(27, PI_OUTPUT);
   LOG(INFO) << "gpio GPIO_TRIGGER_OUT pull down = " << gpioSetPullUpDown(27, PI_PUD_DOWN);
 
-  while(true)
+  while (true)
   {
     sleep(2);
-    LOG(INFO) << "Write GPIO_TRIGGER_OUT High   = " << gpioWrite(27,1);
+    LOG(INFO) << "Write GPIO_TRIGGER_OUT High   = " << gpioWrite(27, 1);
     sleep(2);
-    LOG(INFO) << "Write GPIO_TRIGGER_OUT Low    = " << gpioWrite(27,0);
+    LOG(INFO) << "Write GPIO_TRIGGER_OUT Low    = " << gpioWrite(27, 0);
   }
 
   return 0;
