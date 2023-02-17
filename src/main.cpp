3dc2587a899ee262dc3346a8d014a3d2cd800766
Meng.Zhou <>
2023-02-17 15:10:14 +0800

    add comment;

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include <pigpio.h>
 #include <glog/logging.h>
 
-#define PWM_PIN 21
+#define PWM_PIN 21U
+#define PWM_FRQ 500U // Hz
+#define PWM_DUTY_RANGE 100U
+#define PWM_DUTY 50U
 
 int main(int argc, char **argv)
 {
@@ -15,11 +18,15 @@ int main(int argc, char **argv)
   // set gpio mode = pwm
   gpioSetMode(PWM_PIN, PI_ALT0);
   // set pwm frq
-  gpioSetPWMfrequency(PWM_PIN, 500);
+  gpioSetPWMfrequency(PWM_PIN, PWM_FRQ);
   // set pwm duty range
-  gpioSetPWMrange(PWM_PIN, 100);
+  gpioSetPWMrange(PWM_PIN, PWM_DUTY_RANGE);
   // start pwm by duty
-  gpioPWM(PWM_PIN, 50);
+  gpioPWM(PWM_PIN, PWM_DUTY);
+
+  LOG(INFO) << "PWM is on ,"
+            << " Frequency = " << PWM_FRQ << "Hz ,"
+            << " Duty = " << PWM_DUTY << "/" << PWM_DUTY_RANGE;
 
   while (true)
   {
